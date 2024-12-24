e8970c65e0849d30da83de28ff1eeb62b51be187
Vaden89 <isaacshosanya89@gmail.com>
2024-12-24 10:57:15 +0100

    Added text animations

--- app/components/ProjectDisplayCard.jsx
+++ app/components/ProjectDisplayCard.jsx
@@ -1,6 +1,6 @@
 export const ProjectDisplayCard = () => {
   return (
-    <div className="min-w-full lg:min-w-10 lg:w-1/3 h-[40vh] lg:h-[50vh] bg-[#202020] flex flex-col p-4 rounded-xl gap-2">
+    <div className="w-full h-[40vh] lg:h-[50vh] bg-[#202020] flex flex-col p-4 rounded-xl gap-2 transition-transform ease-in-out duration-200 sm:hover:scale-105 hover:cursor-pointer">
       <h2 className="text-white font-bold text-lg">Dev Links</h2>
       <div className="w-full h-3/5 bg-white rounded-xl"></div>
       <p className=" text-sm font-semibold text-[#777]">
