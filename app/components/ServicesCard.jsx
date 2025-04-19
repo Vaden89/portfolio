2194e22bc9e21a0b6be09c0b50a6a43ee54ee2ca
Vaden89 <isaacshosanya89@gmail.com>
2025-04-19 15:54:26 +0100

    Changes to animation delay, and projectcard sizing for mobile

--- app/components/ServicesCard.jsx
+++ app/components/ServicesCard.jsx
@@ -7,7 +7,7 @@ export const ServicesCard = ({ header, desc }) => {
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
-      transition={{ duration: 1, ease: "easeOut" }}
+      transition={{ duration: 1.5, ease: "easeOut" }}
       className="w-full h-[250px] bg-[#0D1826] px-6"
     >
       <div className="h-1/3 text-xl font-semibold flex flex-col justify-end mb-4">
